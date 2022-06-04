f6cc506436f67290c75495f2a0e891e11f54056a
ojy9612 <pvnm@naver.com>
2022-06-04 16:48:18 +0900

    remove source files

---
+++ src/test/java/com/homework/hanghae99homework01/controller/ContentsControllerTest.java
@@ -0,0 +1,31 @@
+package com.homework.hanghae99homework01.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ContentsControllerTest {
+
+
+
+    @Test
+    void getAllContents() {
+    }
+
+    @Test
+    void createContents() {
+    }
+
+    @Test
+    void getContents() {
+    }
+
+    @Test
+    void updateContents() {
+    }
+
+    @Test
+    void deleteContents() {
+    }
+}
